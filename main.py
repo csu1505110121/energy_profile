d5df08b7dee87cd9da51171c533cafb2b69d91b8
Forty_Braver <csu1505110121@163.com>
2021-08-08 11:33:00 +0800

    revised

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ data = pd.read_excel(parameters['FILENAME'],header=None,usecols=[0,1],sheet_name
 _x_name = data[parameters['SHEET_NAMES'][0]].loc[:,0].values
 y = [data[path].loc[:,1].values for path in parameters['SHEET_NAMES']]
 #print(_x_name)
-print(y)
+#print(y)
 colors = parameters['COLORS']
 delta = DeltaG_plot(_x_name,y,color=colors,\
-						path_labels=parameters['LEGEND_NAMES'],TextLabel='False',\
-						output=parameters['OUTPUT'],\
-						xlabel=parameters['XLABEL'],ylabel=parameters['YLABEL'])
+						path_labels=parameters['LEGEND_NAMES'],\
+						specified_params=parameters)
+
+#print(delta.params)
+#print(delta.ymax)
 delta.min_max()
 delta.create_canves()
 delta.set_label()
